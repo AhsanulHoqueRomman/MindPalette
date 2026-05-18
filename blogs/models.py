c4940f4be734250870eba161f4cbeafbf635265e
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-18 18:42:31 +0600

    Creating new model blogs

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,3 +13,26 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+    
+
+STATUS_CHOICE = (
+    ('Daraft', 'Draft'),
+    ('Published', 'Published')
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='blogs/%Y/%m/%d/')
+    short_description = models.TextField(max_length=300)
+    blog_body = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICE)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
