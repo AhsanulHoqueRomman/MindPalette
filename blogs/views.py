3520841cb992bea488a4b19bb29b6f2f5d061eba
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-20 02:34:12 +0600

    handling category error

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 , redirect
 from django.http import HttpResponse
 from .models import Blog , Category
 
@@ -7,7 +7,16 @@ from .models import Blog , Category
 def posts_by_category(request, category_id):
 
     posts = Blog.objects.filter(category = category_id, status='Published')
-    category = Category.objects.get(pk = category_id )
+    # Use try/except block when we want to do some custom action if the category does not exist.
+    try:
+        category = Category.objects.get(pk = category_id )
+    except:
+        #redirect the user to home.
+        return redirect('home')
+    
+    #Use get_object_or_404 when we want to show 404 error page if the category doesn't exist.
+    # category = get_object_or_404(Category, pk = category_id)
+
     context = {
         'posts' : posts,
         'category' :  category
