2639d2dcc0167e0339a5765b15b2649831f2187e
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-20 02:45:36 +0600

    creating custom error page

--- blogs/views.py
+++ blogs/views.py
@@ -8,14 +8,14 @@ def posts_by_category(request, category_id):
 
     posts = Blog.objects.filter(category = category_id, status='Published')
     # Use try/except block when we want to do some custom action if the category does not exist.
-    try:
-        category = Category.objects.get(pk = category_id )
-    except:
-        #redirect the user to home.
-        return redirect('home')
+    # try:
+    #     category = Category.objects.get(pk = category_id )
+    # except:
+    #     #redirect the user to home.
+    #     return redirect('home')
     
     #Use get_object_or_404 when we want to show 404 error page if the category doesn't exist.
-    # category = get_object_or_404(Category, pk = category_id)
+    category = get_object_or_404(Category, pk = category_id)
 
     context = {
         'posts' : posts,
