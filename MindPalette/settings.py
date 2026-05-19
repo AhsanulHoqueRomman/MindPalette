d6342b03cba4048afda4f6c03b89a5a8705296de
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-20 03:04:06 +0600

    creating base template

--- MindPalette/settings.py
+++ MindPalette/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&n%ukoh16v#1y2$d*=kd=atbi!2eujrfr^afn=34zcmtu6)-47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
