1bb272ef433baca7095c4da2711c9ef78845c69e
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-20 02:19:18 +0600

    Implementing pk based post by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog , Category
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+
+    posts = Blog.objects.filter(category = category_id, status='Published')
+    category = Category.objects.get(pk = category_id )
+    context = {
+        'posts' : posts,
+        'category' :  category
+    }
+
+    return render(request, 'posts_by_category.html', context)
