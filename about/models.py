c588f78a547470455cd7d30dc2f444c25d4a726b
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-20 21:05:26 +0600

    adding dynamic about Us Section

--- about/models.py
+++ about/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class About(models.Model):
     about_heading = models.CharField(max_length=55)
     about_description = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'About'
 
     def __str__(self):
         return self.about_heading
+    
+
+    
