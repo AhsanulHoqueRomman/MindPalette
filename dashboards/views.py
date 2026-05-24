bb1b2a14dd0502d4edfb885bd32b45a8cbf7e29b
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-25 02:59:10 +0600

    customizing dashboard and created categories table

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blogs.models import Category, Blog
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html' )
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+    context ={
+        'category_count' : category_count,
+        'blogs_count' : blogs_count,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
+
+def categories(request):
+    return render(request, 'dashboard/categories.html')
